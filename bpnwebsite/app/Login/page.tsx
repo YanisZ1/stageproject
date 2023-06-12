56811e375b20f45874e6893ee1f23ccd5bdc4f22
YanisZ1 <yanis.zekk@gmail.com>
2023-06-12 11:38:08 +0200

    Ajout du Tailwind css dans la page de login

--- bpnwebsite/app/Login/page.tsx
+++ bpnwebsite/app/Login/page.tsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React from 'react';
 
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-const Login=()=>{
-	return(
-	<div>
-	<form action=""> 
-		<div> 
-		<label htmlFor="email">Email</label>
-		<input type="text" name="email" id="email"/> 
-		</div> 
-			<div> 
-				<label htmlFor="passw">Password</label>
-				<input type="text" name="passw" id="passw"/> 
-			</div>  
-				<button type="submit">Login</button>
-			</form>
-		</div>
-	)
-}
-export default Login
+export default Login;
